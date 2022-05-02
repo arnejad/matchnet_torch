b738fa007aed136e94c1608b48a29d4c8f3a87a6
Ashkan Nejad <rahmani.nejad.ali@gmail.com>
2022-05-02 16:42:13 +0200

    data loading and combining added

--- train.sh
+++ train.sh
@@ -12,4 +12,11 @@ module load scikit-image/0.16.2-fosscuda-2019b-Python-3.7.4
 
 source /data/p306627/.envs/torch/bin/activate
 
-python3 /home/p306627/codes/matchnet_torch/train.py
+cp /data/p306627/DBs/matchnet.tar.xz /local/tmp/
+
+
+python3 /home/p306627/codes/matchnet_torch/train.py 
+
+
+rm -r /local/tmp/matchnet
+rm /local/tmp/matchnet.tar.xz
